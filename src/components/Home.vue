e2ee4a10a5c262dccf524edcc3cd1de31f868ae8
guisoacelga <guiso.acelga@gmail.com>
2020-07-17 13:13:19 +0200

    Refactor cards in Home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,38 +1,15 @@
 <template>
-<div>
-  <Slider />
-  <!-- ****** Cards ***** -->
-  <div class="row mt-20">
-    <Card
-      image="img-1.jpg"
-      title="Eiffel Tower"
-      text="The Eiffel Tower is a wrought iron lattice tower on the Champ de Mars in Paris, France."
-    />
-    <Card
-      image="img-2.jpg"
-      title="Big Ben"
-      text="Big Ben is the nickname for the Great Bell of the clock at the north end of the Palace of Westminster in London."
-    />
-    <Card
-      image="img-3.jpg"
-      title="Statue of Liberty"
-      text="The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor in New York City, in the United States."
-    />
-    <Card
-      image="img-4.jpg"
-      title="Sydney Opera House"
-      text="The Sydney Opera House is a multi-venue performing arts centre in Sydney, Australia. It is one of the 20th century's most famous and distinctive buildings."
-    />
-    <Card
-      image="img-5.jpg"
-      title="Parliament Hill"
-      text="Parliament Hill, colloquially known as The Hill, is an area of Crown land on the southern banks of the Ottawa River in downtown Ottawa, Ontario."
-    />
-    <Card
-      image="img-6.jpg"
-      title="Great Wall of China"
-      text="The Great Wall of China is a series of fortifications made of stone, brick, rammed earth, wood, and other materials, generally built to protect the Chinese states and empires against the raids and invasions."
-    />
+  <div>
+    <Slider />
+    <!-- ****** Cards ***** -->
+    <div class="row mt-20">
+      <Card 
+      v-for="card in cards"
+      :key="card.id"
+        :image="card.image"
+        :title="card.title"
+        :text="card.text"
+      />
     </div>
   </div>
 </template>
@@ -45,6 +22,54 @@ export default {
   components: {
     Slider,
     Card
+  },
+  data: function() {
+    return {
+      cards: [
+        {
+          id: 1,
+          image: "img-1.jpg",
+          title: "Eiffel Tower",
+          text:
+            "The Eiffel Tower is a wrought iron lattice tower on the Champ de Mars in Paris, France."
+        },
+        {
+          id: 2,
+          image: "img-2.jpg",
+          title: "Big Ben",
+          text:
+            "Big Ben is the nickname for the Great Bell of the clock at the north end of the Palace of Westminster in London."
+        },
+        {
+          id: 3,
+          image: "img-3.jpg",
+          title: "Statue of Liberty",
+          text:
+            "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor in New York City, in the United States."
+        },
+        {
+          id: 4,
+          image: "img-4.jpg",
+          title: "Sydney Opera House",
+          text:
+            "The Sydney Opera House is a multi-venue performing arts centre in Sydney, Australia. It is one of the 20th century's most famous and distinctive buildings."
+        },
+        {
+          id: 5,
+          image: "img-5.jpg",
+          title: "Parliament Hill",
+          text:
+            "Parliament Hill, colloquially known as The Hill, is an area of Crown land on the southern banks of the Ottawa River in downtown Ottawa, Ontario."
+        },
+        {
+          id: 6,
+          image: "img-6.jpg",
+          title: "Great Wall of China",
+          text:
+            "The Great Wall of China is a series of fortifications made of stone, brick, rammed earth, wood, and other materials, generally built to protect the Chinese states and empires against the raids and invasions."
+        }
+      ]
+    };
   }
 };
 </script>
